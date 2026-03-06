5a284d7b91f55d46f02465f6e7187b121900e0ed
Aurelien Deutchoua <aurelien.deutchoua@epitech.eu>
2026-03-06 11:27:18 +0100

    modification index.php

--- index.php
+++ index.php
@@ -7,7 +7,8 @@ if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
     http_response_code(200);
     exit();
 }
-require_once __DIR__ . "/config/database.php"; // inclusion  de la base de données
+
+require_once __DIR__ . "/backend/config/database.php"; // inclusion  de la base de données
 // inclusion des différents fichiers
 // require_once "models/Movie.php";
 // require_once "repositories/MovieRepository.php" ;

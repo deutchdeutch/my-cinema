5ddd22c493a4b42c882a3a91521e660de4884398
Aurelien Deutchoua <aurelien.deutchoua@epitech.eu>
2026-03-06 13:38:23 +0100

    mon projet académique my cinema

--- backend/index.php
+++ backend/index.php
@@ -1,5 +1,5 @@
 <?php
-header("Access-Control-Allow-Origin: https://ton-pseudo.github.io");
+header("Access-Control-Allow-Origin: http://127.0.0.1:5500");
 header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
 header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
 header('Content-Type: application/json; charset=UTF-8');
@@ -7,9 +7,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
     http_response_code(200);
     exit();
 }
-
-require_once __DIR__ . '/config/database.php'; // inclusion  de la base de données
-
+require_once __DIR__ . "/config/database.php"; // inclusion  de la base de données
 // inclusion des différents fichiers
 // require_once "models/Movie.php";
 // require_once "repositories/MovieRepository.php" ;

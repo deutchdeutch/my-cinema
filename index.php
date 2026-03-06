ef90c5ea42eca9c2e4fba488640bbd5b6d1e7b9a
Aurelien Deutchoua <aurelien.deutchoua@epitech.eu>
2026-03-06 12:07:52 +0100

    deplacement des fichiers à la racine

--- index.php
+++ index.php
@@ -8,7 +8,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
     exit();
 }
 
-require_once __DIR__ . "/backend/config/database.php"; // inclusion  de la base de données
+require_once __DIR__ . "/config/database.php"; // inclusion  de la base de données
 // inclusion des différents fichiers
 // require_once "models/Movie.php";
 // require_once "repositories/MovieRepository.php" ;
